525a525c4eeb12fcf87a6b569a79a9cbd65b29ca
Remallya-16 <kremallya9@gmail.com>
2025-11-27 15:03:10 +0530

    Create Temperature.py

---
+++ Temperature.py
@@ -0,0 +1,17 @@
+import sys
+if len(sys.argv) == 2
+    temp = float(sys.argv[1])
+    print("user provided temperature:")
+else:
+    temp = 25
+    print("No input given - using default temperature:")
+if temp < 15:
+    status = "Cold"
+elif temp <= 30:
+    status = "Normal"
+else:
+    status = "Hot"
+
+print("Temperature Status:", status)
+ 
+
